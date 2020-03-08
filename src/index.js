74cc3bdb2a2d02a8e917b1c9948a51ee8ad21964
Manoj Chalode <chalodem@gmail.com>
2020-03-08 18:09:27 +0000

    added request url logging

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const { createServer } = require("http");
 const { createReadStream } = require("fs");
 const { join } = require("path");
 const handler = (type, error) => console.error(type, error.stack || error);
-const server = createServer((request, response) => createReadStream(join(__dirname, "index.html")).pipe(response));
+const server = createServer((request, response) => {
+    console.log(`incoming request - ${require.url}`);
+    createReadStream(join(__dirname, "index.html")).pipe(response);
+});
 server.listen(8080, () => { console.log(`app started on port - 8080`); });
 process.on("unhandledRejection", (reason, promise) => handler("unhandledRejection", reason))
     .on("uncaughtException", (error) => handler("uncaughtException", error));
