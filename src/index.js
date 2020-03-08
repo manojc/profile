53d45e81c9168875e115f09cc6dbb5d99dcdf45d
Manoj Chalode <chalodem@gmail.com>
2020-03-08 16:38:04 +0000

    added simple app.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { createServer } = require("http");
+const { createReadStream } = require("fs");
+const { join } = require("path");
+const handler = (type, error) => console.error(type, error.stack || error);
+const server = createServer((request, response) => createReadStream(join(__dirname, "index.html")).pipe(response));
+server.listen(8080, () => { console.log(`app started on port - 8080`); });
+process.on("unhandledRejection", (reason, promise) => handler("unhandledRejection", reason))
+    .on("uncaughtException", (error) => handler("uncaughtException", error));
